200f055b416ec10e6e6d85439698ca6887b635da
yaorenjie <yaorenjie@hifive.ai>
2022-07-12 20:06:36 +0800

    添加地图

--- App.vue
+++ App.vue
@@ -11,6 +11,11 @@
 		},
 		onNavigationBarButtonTap(e) {
 			console.log('1111')
+		},
+		mounted () {
+			// var scriptElement = document.createElement('script');
+			// document.body.appendChild(scriptElement);
+			// scriptElement.src = "https://map.qq.com/api/gljs?v=1.exp&key=TKUBZ-D24AF-GJ4JY-JDVM2-IBYKK-KEBCU"
 		}
 	}
 </script>
